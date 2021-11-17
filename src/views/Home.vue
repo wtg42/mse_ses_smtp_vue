4b418e66f534582743220612b5ce59858b64598e
weiting <weiting.shi1982@gmail.com>
2021-11-17 16:56:10 +0800

    ### 版面調整囉

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="flex justify-center mt-10">
+  <main class="flex justify-center mt-10">
+  <div class="flex flex-col justify-center mt-10">
     <section class="topic">
       <div class="flex items-center -ml-4">
         <div class="mb-2"><i class="nes-jp-logo transform scale-50"></i></div>
@@ -13,7 +14,19 @@
       <p>Just send a test mail to MSE.</p>
       <p>If you want to add IP, please edit db.json file.</p>
     </section>
+    <section class="icon-list mt-5">
+      <!-- Copyright Nintendo -->
+      <i class="nes-mario"></i>
+      <i class="nes-ash"></i>
+      <i class="nes-pokeball"></i>
+      <i class="nes-bulbasaur"></i>
+      <i class="nes-charmander"></i>
+      <i class="nes-squirtle"></i>
+      <i class="nes-kirby"></i>
+    </section>
   </div>
+  </main>
+  
   
 </template>
 
