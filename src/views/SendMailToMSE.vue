7fe0537dbb0e3f826c4d981135690530200f31c9
weiting <weiting.shi1982@gmail.com>
2021-12-10 17:00:05 +0800

    ### Dialog Modal Component  - Now app can pop dialog modal after send mail successed or failed.  - dynamically modal contents by using vue emit function

--- src/views/SendMailToMSE.vue
+++ src/views/SendMailToMSE.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="py-12">
     <div v-if="ipList.data && ipList.data.length > 0" class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <BasicTextForm :ipList="ipList.data" />
+      <!-- 表單送出後通知 RoundedDialog 順便帶入要顯示的資料 -->
+      <BasicTextForm :ipList="ipList.data" @showModal="togglesModal" />
     </div>
     <div v-else-if="!ipList.data && !error" class="max-w-7xl mx-auto sm:px-24 lg:px-24">
       <progress class="nes-progress is-pattern" :value="progress" max="100"></progress>
@@ -9,22 +10,45 @@
     <div v-else-if="error" class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-center">
       <p>{{ error }}</p>
     </div>
+    <teleport to="body">
+        <RoundedDialog ref="roundedDialogRef" v-model:dialogRequest="dialogRequest"/>
+    </teleport>
   </div>
 </template>
 
 <script>
 import BasicTextForm from "@/components/BasicTextForm.vue";
+import RoundedDialog from "@/components/RoundedDialog.vue";
 import getMseIpLists from "@/composables/getMseIpLists.js"
+import { ref } from "vue"
 export default {
   name: "SendMailToMSE",
   components: {
     BasicTextForm,
+    RoundedDialog,
   },
   setup() {
     // 跟資料庫要機器 IP 資料顯示
     const { progress, ipList, error, load } = getMseIpLists()
+    // 打 API
     load()
-    return { ipList, error, progress }
+
+    // 要求顯示的內容
+    const dialogRequest = ref({titile: ref(""), body: ref("")})
+
+    // 參照變數
+    const roundedDialogRef = ref()
+
+    // 通知 dialog 顯示並帶入資訊
+    const togglesModal = (data) => {
+      // parent 呼叫 dialog 
+      dialogRequest.value = data
+      console.log(dialogRequest)
+      // 呼叫 Component 內部 showModal()
+      roundedDialogRef.value.showModal()
+    };
+
+    return { ipList, error, progress, togglesModal, roundedDialogRef, dialogRequest }
   }
 }
 </script>
